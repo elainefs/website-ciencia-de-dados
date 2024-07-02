1fd677f7c31634a22f68235805a6d7409b6f06ec
Elaine Ferreira <elaineferreira.dev@gmail.com>
2024-07-02 13:25:34 -0300

    Add DB files

---
+++ db/conexao_variaveis.php
@@ -0,0 +1,10 @@
+<!--
+Dados padrões utilizados pelo XAMPP 
+Criar banco com mesmo nome do dbname ou altera-lo se preferir. 
+-->
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_chamados";
+?>
